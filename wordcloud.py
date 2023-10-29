5d3ac0e4d70f079d7e608eccec7de8fb66907704
Marian <74514827+pretty1020@users.noreply.github.com>
2023-10-30 04:34:21 +0800

    Update wordcloud.py

--- wordcloud.py
+++ wordcloud.py
@@ -36,7 +36,7 @@ def set_background(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
-set_background('C:/Users/admin/OneDrive/Desktop/wfmcalc3.png')
+set_background('wfmcalc3.png')
 
 # Define the Streamlit app title and description
 st.title("Word Cloud and Sentiment Analysis")
