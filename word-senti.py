8f062a4a547763ae622a7c89cbb57dafebe78be8
Marian <74514827+pretty1020@users.noreply.github.com>
2025-03-17 11:13:05 +0800

    Update word-senti.py

--- word-senti.py
+++ word-senti.py
@@ -13,21 +13,6 @@ def get_base64(bin_file):
     return base64.b64encode(data).decode()
 
 
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    color: gold; /* Change font color to purple */
-    background-attachment: local;
-    background-position: auto;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 set_background('senti1.png')
 
