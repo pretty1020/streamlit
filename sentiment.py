2c2ef255734b70461d891a02608c8adc5e2d3d41
Marian <74514827+pretty1020@users.noreply.github.com>
2025-03-17 10:03:58 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -8,16 +8,6 @@ import subprocess
 import spacy
 from io import BytesIO
 
-# Ensure SpaCy model is installed
-def ensure_spacy_model():
-    try:
-        nlp = spacy.load("en_core_web_sm")
-    except OSError:
-        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
-        nlp = spacy.load("en_core_web_sm")
-    return nlp
-
-nlp = ensure_spacy_model()
 
 
 # Function to analyze sentiment
