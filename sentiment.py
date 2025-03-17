a406c5fb3cfb20f1c50293812990231dd363a3b4
Marian <74514827+pretty1020@users.noreply.github.com>
2025-03-17 10:34:06 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -10,6 +10,15 @@ import spacy
 from io import BytesIO
 from PIL import ImageFont
 
+import subprocess
+import sys
+
+# Ensure the latest version of pillow is installed
+try:
+    import PIL
+except ImportError:
+    subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pillow"])
+    import PIL
 
 
 
