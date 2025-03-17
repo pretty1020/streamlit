929893b81c376c35a83c79be47617de85403b64f
Marian <74514827+pretty1020@users.noreply.github.com>
2025-03-17 10:48:42 +0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -23,12 +23,7 @@ except ImportError:
     subprocess.run([sys.executable, "-m", "pip", "install", "spacy"])
     import spacy
 
-# Ensure the SpaCy model is available
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
-    nlp = spacy.load("en_core_web_sm")
+
 
 # Function to analyze sentiment
 def analyze_sentiment(text):
